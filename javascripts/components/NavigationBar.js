74fad1ec5df2dbd254fa7e272859cf44b4884f8f
Nafisa <amirnafisa@gmail.com>
2020-03-24 11:55:06 -0400

    Added the images to github

--- javascripts/components/NavigationBar.js
+++ javascripts/components/NavigationBar.js
@@ -22,10 +22,10 @@ class NavigationBar extends React.Component {
                 <tbody>
                 <tr>
                     <td className="title">Nafisa Ali Amir</td>
-                    <td className={this.props.selection===1?"selected margin10 nav-menu":"margin10 nav-menu"} onClick={this.selectHome.bind(this)}>Home</td>
-                    <td className={this.props.selection===2?"selected margin10 nav-menu":"margin10 nav-menu"} onClick={this.selectProjects.bind(this)}>Projects</td>
-                    <td className={this.props.selection===3?"selected margin10 nav-menu":"margin10 nav-menu"} onClick={this.selectResume.bind(this)}>Resume</td>
-                    <td className={this.props.selection===4?"selected margin10 nav-menu":"margin10 nav-menu"} onClick={this.selectContact.bind(this)}>Contact</td>
+                    <td className={this.props.selection===1?"selected nav-menu":"nav-menu"} onClick={this.selectHome.bind(this)}>Home</td>
+                    <td className={this.props.selection===2?"selected nav-menu":"nav-menu"} onClick={this.selectProjects.bind(this)}>Projects</td>
+                    <td className={this.props.selection===3?"selected nav-menu":"nav-menu"} onClick={this.selectResume.bind(this)}>Resume</td>
+                    <td className={this.props.selection===4?"selected nav-menu":"nav-menu"} onClick={this.selectContact.bind(this)}>Contact</td>
                 </tr>
                 </tbody>
             </table>
